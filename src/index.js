a6020b4dcedde95f75359f1a1c35c8464a0af401
cupcakearmy <hi@nicco.io>
2020-05-11 00:53:00 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,54 @@
+const cp = require('child_process')
+const chalk = require('chalk')
+
+const inquirer = require('inquirer')
+
+function checkIfBrewIsInstalled() {
+  try {
+    cp.execSync('brew --version')
+    return true
+  } catch (e) {
+    return false
+  }
+}
+
+function getListOfLeaves() {
+  const list = cp.execSync('brew leaves', { encoding: 'utf-8' })
+  return list.trim().split('\n')
+}
+
+function getLoosers(keepers) {
+  return getListOfLeaves().filter((leave) => !keepers.includes(leave))
+}
+
+async function main() {
+  if (!checkIfBrewIsInstalled()) {
+    console.log(chalk.red.underline('Brew not installed'))
+    return
+  }
+
+  const { keepers } = await inquirer.prompt([
+    {
+      type: 'checkbox',
+      message: 'Uncheck all unwanted packages',
+      name: 'keepers',
+      choices: getListOfLeaves().map((leave) => ({
+        name: leave,
+        checked: true,
+      })),
+    },
+  ])
+
+  console.log('🗑 Uninstalling:', chalk.bold.blue(getLoosers(keepers).join(' ')))
+  // while (true) {
+  //   const loosers = getLoosers(keepers)
+  //   if (!loosers.length) break
+
+  //   const joinedLoosers = loosers.join(' ')
+  //   cp.execSync(`brew uninstall ${joinedLoosers}`)
+  // }
+  console.log('🧽 Cleaning up')
+  cp.execSync(`brew cleanup`)
+  console.log(chalk.bold.green('🚀 Done'))
+}
+main()
